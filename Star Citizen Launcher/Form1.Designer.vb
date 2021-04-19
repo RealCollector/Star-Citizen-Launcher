d8f79e8d40e5dfe22cefda22f645bac7f8faffd9
RealCollector <realcollectorgaming@gmail.com>
2021-04-19 00:11:20 -0600

    First Commit

---
+++ Star Citizen Launcher/Form1.Designer.vb
@@ -0,0 +1,168 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Btn_GetLaunchPath = New System.Windows.Forms.Button()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Btn_GetGamePath = New System.Windows.Forms.Button()
+        Me.Btn_Backup = New System.Windows.Forms.Button()
+        Me.FolderBrowserL = New System.Windows.Forms.FolderBrowserDialog()
+        Me.Lbl_LaunchPath = New System.Windows.Forms.Label()
+        Me.Btn_Launch = New System.Windows.Forms.Button()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.Lbl_GamePath = New System.Windows.Forms.Label()
+        Me.Lbl_BackupPath = New System.Windows.Forms.Label()
+        Me.SuspendLayout()
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Font = New System.Drawing.Font("Arial", 14.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point)
+        Me.Label1.Location = New System.Drawing.Point(69, 38)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(208, 22)
+        Me.Label1.TabIndex = 1
+        Me.Label1.Text = "Select Launcher Path"
+        '
+        'Btn_GetLaunchPath
+        '
+        Me.Btn_GetLaunchPath.Location = New System.Drawing.Point(283, 34)
+        Me.Btn_GetLaunchPath.Name = "Btn_GetLaunchPath"
+        Me.Btn_GetLaunchPath.Size = New System.Drawing.Size(127, 34)
+        Me.Btn_GetLaunchPath.TabIndex = 2
+        Me.Btn_GetLaunchPath.Text = "Browse"
+        Me.Btn_GetLaunchPath.UseVisualStyleBackColor = True
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Font = New System.Drawing.Font("Arial", 14.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point)
+        Me.Label2.Location = New System.Drawing.Point(104, 106)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(173, 22)
+        Me.Label2.TabIndex = 3
+        Me.Label2.Text = "Select Game Path"
+        '
+        'Btn_GetGamePath
+        '
+        Me.Btn_GetGamePath.Location = New System.Drawing.Point(283, 99)
+        Me.Btn_GetGamePath.Name = "Btn_GetGamePath"
+        Me.Btn_GetGamePath.Size = New System.Drawing.Size(127, 34)
+        Me.Btn_GetGamePath.TabIndex = 4
+        Me.Btn_GetGamePath.Text = "Browse"
+        Me.Btn_GetGamePath.UseVisualStyleBackColor = True
+        '
+        'Btn_Backup
+        '
+        Me.Btn_Backup.Location = New System.Drawing.Point(283, 156)
+        Me.Btn_Backup.Name = "Btn_Backup"
+        Me.Btn_Backup.Size = New System.Drawing.Size(127, 34)
+        Me.Btn_Backup.TabIndex = 5
+        Me.Btn_Backup.Text = "Browse"
+        Me.Btn_Backup.UseVisualStyleBackColor = True
+        '
+        'Lbl_LaunchPath
+        '
+        Me.Lbl_LaunchPath.AutoSize = True
+        Me.Lbl_LaunchPath.BackColor = System.Drawing.SystemColors.HighlightText
+        Me.Lbl_LaunchPath.Location = New System.Drawing.Point(430, 45)
+        Me.Lbl_LaunchPath.Name = "Lbl_LaunchPath"
+        Me.Lbl_LaunchPath.Size = New System.Drawing.Size(73, 15)
+        Me.Lbl_LaunchPath.TabIndex = 6
+        Me.Lbl_LaunchPath.Text = "asdfasdfasdf"
+        '
+        'Btn_Launch
+        '
+        Me.Btn_Launch.Font = New System.Drawing.Font("Segoe UI", 12.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point)
+        Me.Btn_Launch.ForeColor = System.Drawing.Color.Blue
+        Me.Btn_Launch.Location = New System.Drawing.Point(283, 220)
+        Me.Btn_Launch.Name = "Btn_Launch"
+        Me.Btn_Launch.Size = New System.Drawing.Size(127, 34)
+        Me.Btn_Launch.TabIndex = 7
+        Me.Btn_Launch.Text = "Run SC"
+        Me.Btn_Launch.UseVisualStyleBackColor = True
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Font = New System.Drawing.Font("Arial", 14.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point)
+        Me.Label3.Location = New System.Drawing.Point(48, 160)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(229, 22)
+        Me.Label3.TabIndex = 8
+        Me.Label3.Text = "Select Backup Location"
+        '
+        'Lbl_GamePath
+        '
+        Me.Lbl_GamePath.AutoSize = True
+        Me.Lbl_GamePath.BackColor = System.Drawing.SystemColors.HighlightText
+        Me.Lbl_GamePath.Location = New System.Drawing.Point(430, 109)
+        Me.Lbl_GamePath.Name = "Lbl_GamePath"
+        Me.Lbl_GamePath.Size = New System.Drawing.Size(95, 15)
+        Me.Lbl_GamePath.TabIndex = 10
+        Me.Lbl_GamePath.Text = "asdfasdfasdfasdf"
+        '
+        'Lbl_BackupPath
+        '
+        Me.Lbl_BackupPath.AutoSize = True
+        Me.Lbl_BackupPath.BackColor = System.Drawing.SystemColors.HighlightText
+        Me.Lbl_BackupPath.Location = New System.Drawing.Point(430, 166)
+        Me.Lbl_BackupPath.Name = "Lbl_BackupPath"
+        Me.Lbl_BackupPath.Size = New System.Drawing.Size(95, 15)
+        Me.Lbl_BackupPath.TabIndex = 11
+        Me.Lbl_BackupPath.Text = "asdfasdfasdfasdf"
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(7.0!, 15.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(1339, 329)
+        Me.Controls.Add(Me.Lbl_BackupPath)
+        Me.Controls.Add(Me.Lbl_GamePath)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.Btn_Launch)
+        Me.Controls.Add(Me.Lbl_LaunchPath)
+        Me.Controls.Add(Me.Btn_Backup)
+        Me.Controls.Add(Me.Btn_GetGamePath)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Btn_GetLaunchPath)
+        Me.Controls.Add(Me.Label1)
+        Me.Name = "Form1"
+        Me.Text = "Star Citizen Easy Launch "
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Btn_GetLaunchPath As Button
+    Friend WithEvents Label2 As Label
+    Friend WithEvents Btn_GetGamePath As Button
+    Friend WithEvents Btn_Backup As Button
+    Friend WithEvents FolderBrowserL As FolderBrowserDialog
+    Friend WithEvents Lbl_LaunchPath As Label
+    Friend WithEvents Btn_Launch As Button
+    Friend WithEvents Label3 As Label
+    Friend WithEvents Lbl_GamePath As Label
+    Friend WithEvents Lbl_BackupPath As Label
+End Class
---
+++ Star Citizen Launcher/Form1.vb
@@ -0,0 +1,255 @@
+﻿Imports System.IO
+Imports System.Collections
+Public Class Form1
+
+
+
+
+    Public lDir As String   'launcher path
+    Public gDir As String   'game directory path
+    Public bDir As String   'backup directory path
+    Public cFile As String = "F:\VIsual Studio Stuff\Star Citizen Launcher\config.txt"    '"C:\Program Files (x86)\Easy SC Launch\config.txt"   'config directory
+    Public tFile As String = "F:\VIsual Studio Stuff\Star Citizen Launcher\temp0.txt"
+
+
+
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+
+
+        VerifyConfig() 'verifys file exists and stores data to public variables
+        VerifyBackupDir()   'verify/create backup directory
+
+        'write data from temp0.txt to config.txt
+        'delete temp0.txt
+
+
+
+
+    End Sub
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Btn_GetLaunchPath.Click
+
+        If FolderBrowserL.ShowDialog() = DialogResult.OK Then
+            lDir = FolderBrowserL.SelectedPath
+            Lbl_LaunchPath.Text = lDir
+            UpdateConfig()
+        End If
+
+    End Sub
+    Private Sub Btn_GetGamePath_Click(sender As Object, e As EventArgs) Handles Btn_GetGamePath.Click
+
+        If FolderBrowserL.ShowDialog() = DialogResult.OK Then
+            gDir = FolderBrowserL.SelectedPath
+            Lbl_GamePath.Text = gDir
+            UpdateConfig()
+        End If
+
+    End Sub
+    Private Sub Btn_Backup_Click(sender As Object, e As EventArgs) Handles Btn_Backup.Click
+
+        If FolderBrowserL.ShowDialog() = DialogResult.OK Then
+            bDir = FolderBrowserL.SelectedPath
+            Lbl_BackupPath.Text = bDir
+            UpdateConfig()
+        End If
+
+    End Sub
+    Private Sub Btn_Launch_Click(sender As Object, e As EventArgs) Handles Btn_Launch.Click
+        BackupFiles()
+    End Sub
+    Private Sub VerifyConfig()
+        If File.Exists(cFile) Then
+            'make sure files exist and read the saved data into the path variables
+            ReadConfig() 'Gets data from stored file
+        Else
+            'create the config file and set default paths
+            Dim stp As New StreamWriter(cFile, True)
+            stp.WriteLine("D:\Program Files\Roberts Space Industries\RSI Launcher") 'Launcher Path
+            stp.WriteLine("D:\Program Files\Roberts Space Industries\Star Citizen") 'Game Folder Path
+            stp.WriteLine("D:\Program Files\Roberts Space Industries\SC-Backups") 'backup path
+            stp.Close()
+            ReadConfig()
+        End If
+    End Sub
+    Private Sub ReadConfig()    'reads config file into memory and to array, writes to temp0 file
+        Dim lineArray() As String = System.IO.File.ReadAllLines(cFile) 'pull lines into array
+        'write lines to public variables
+        lDir = lineArray(0)
+        gDir = lineArray(1)
+        bDir = lineArray(2)
+
+
+        'write data to the temp0 config file
+        If System.IO.File.Exists(tFile) Then 'check if file exists and delete it if its already there
+            File.Delete(tFile)
+        Else
+            'create the config file
+            File.WriteAllLines(tFile, lineArray)
+
+        End If
+
+        'write the path data to the readout box
+        Lbl_LaunchPath.Text = lDir
+        Lbl_GamePath.Text = gDir
+        Lbl_BackupPath.Text = bDir
+
+    End Sub
+
+    Private Sub UpdateConfig()
+        Dim lineArray(3) As String
+        lineArray(0) = lDir
+        lineArray(1) = gDir
+        lineArray(2) = bDir
+
+        If File.Exists(tFile) Then 'check if file exists and delete it if its already there
+            File.Delete(tFile)
+        End If
+
+        'create the config file
+        File.WriteAllLines(tFile, lineArray)
+    End Sub
+
+    Private Sub VerifyBackupDir()
+        If Directory.Exists(bDir) Then
+            'dont worry about it just move on
+        Else
+            Directory.CreateDirectory(bDir)
+        End If
+
+
+    End Sub
+
+    Private Sub BackupFiles()
+        Dim PtuMap As String = "\PTU\USER\Controls\Mappings"
+        Dim LiveMap As String = "\LIVE\USER\Controls\Mappings"
+
+        Dim PtuShade As String = "\PTU\USER\shaders"
+        Dim LiveShade As String = "\LIVE\USER\shaders"
+
+        Dim PtuMap313 As String = "\PTU\USER\Client\0\Controls\Mappings"
+        Dim LiveMap313 As String = "\LIVE\USER\Client\0\Controls\Mappings"
+
+        Dim PtuShade313 As String = "\PTU\USER\Client\0\shaders"
+        Dim LiveShade313 As String = "\LIVE\USER\Client\0\shaders"
+
+        Dim SrcPath As String
+        Dim DestPath As String
+
+        'backkup mapping files from live
+        SrcPath = gDir + LiveMap
+        DestPath = bDir + LiveMap
+        If System.IO.Directory.Exists(SrcPath) Then
+            CopyDirectory(SrcPath, DestPath) 'copy mapping files from live to backup
+            DeleteFiles(SrcPath) 'delete old files from live
+            CopyDirectory(DestPath, SrcPath) 'copy mapping files from backup to live
+        End If
+
+        'delete shaders from live
+        SrcPath = gDir + LiveShade
+        If System.IO.Directory.Exists(SrcPath) Then
+            DeleteShaders(SrcPath) 'delete Shader Directory
+        End If
+
+        'backup mapping files from PTU
+        SrcPath = gDir + PtuMap
+        DestPath = bDir + PtuMap
+        If System.IO.Directory.Exists(SrcPath) Then
+            CopyDirectory(SrcPath, DestPath) 'copy mapping files from PTU to backup
+            DeleteFiles(SrcPath) 'delete old files from PTU
+            CopyDirectory(DestPath, SrcPath) 'copy mapping files from backup to PTU
+        End If
+
+        'delete shaders from PTU
+        SrcPath = gDir + PtuShade
+        If System.IO.Directory.Exists(SrcPath) Then
+            DeleteShaders(SrcPath) 'delete Shader Directory
+        End If
+
+
+        'Backup Live mapping for Assumed live post 3.13+ directory structure
+        SrcPath = gDir + LiveMap313
+        DestPath = bDir + LiveMap313
+        If System.IO.Directory.Exists(SrcPath) Then
+            CopyDirectory(SrcPath, DestPath) 'copy mapping files from 3.13+ Live
+            DeleteFiles(SrcPath) 'delete old files from 3.13+ Live
+            CopyDirectory(DestPath, SrcPath) 'copy mapping files from backup to 3.13 Live
+        End If
+
+        'delete shaders from live 3.13+ Directory Structure
+        SrcPath = gDir + LiveShade313
+        If System.IO.Directory.Exists(SrcPath) Then
+            DeleteShaders(SrcPath) 'delete Shader Directory
+        End If
+
+        'Backup PTU Mapping for assumed post 3.13+ Directory Structure 
+        SrcPath = gDir + PtuMap313
+        DestPath = bDir + PtuMap313
+        If System.IO.Directory.Exists(SrcPath) Then
+            CopyDirectory(SrcPath, DestPath) 'copy mapping files from 3.13+ PTU
+            DeleteFiles(SrcPath) 'delete old files from 3.13+ PTU
+            CopyDirectory(DestPath, SrcPath) 'copy mapping files from backup to 3.13 PTU
+        End If
+
+        'delete shaders from PTU 3.13+
+        SrcPath = gDir + PtuShade313
+        If System.IO.Directory.Exists(SrcPath) Then
+            DeleteShaders(SrcPath) 'delete Shader Directory
+        End If
+
+
+
+    End Sub
+    Public Function DeleteFiles(ByVal path As String)
+        If Directory.Exists(path) Then
+            'Delete all files from the Directory
+            For Each filepath As String In Directory.GetFiles(path)
+                File.Delete(filepath)
+            Next
+        End If
+    End Function
+
+    Public Function DeleteShaders(ByVal path As String)
+        If Directory.Exists(path) Then
+            'Delete all files from the Directory
+            Directory.Delete(path, True)
+        End If
+    End Function
+
+
+    Public Function CopyDirectory(ByVal SrcPath As String, ByVal DestPath As String, Optional _
+ByVal bQuiet As Boolean = False) As Boolean
+        If Not System.IO.Directory.Exists(SrcPath) Then
+            Exit Function
+            'Throw New System.IO.DirectoryNotFoundException("The directory " & SrcPath & " does not exists")
+        End If
+        'If System.IO.Directory.Exists(DestPath) AndAlso Not bQuiet Then
+        '    If MessageBox.Show("directory " & DestPath & " already exists." & vbCrLf &
+        '    "If you continue, any files with the same name will be overwritten",
+        '    "Continue?", MessageBoxButtons.OKCancel, MessageBoxIcon.Question,
+        '    MessageBoxDefaultButton.Button2) = Windows.Forms.DialogResult.Cancel Then Exit Function
+        'End If
+
+        'add Directory Seperator Character (\) for the string concatenation shown later
+        If DestPath.Substring(DestPath.Length - 1, 1) <> System.IO.Path.DirectorySeparatorChar Then
+            DestPath += Path.DirectorySeparatorChar
+        End If
+        If Not System.IO.Directory.Exists(DestPath) Then System.IO.Directory.CreateDirectory(DestPath)
+        Dim Files As String()
+        Files = System.IO.Directory.GetFileSystemEntries(SrcPath)
+        Dim element As String
+        For Each element In Files
+            If System.IO.Directory.Exists(element) Then
+                'if the current FileSystemEntry is a directory,
+                'call this function recursively
+                CopyDirectory(element, DestPath & System.IO.Path.GetFileName(element), True)
+            Else
+                'the current FileSystemEntry is a file so just copy it
+                System.IO.File.Copy(element, DestPath & System.IO.Path.GetFileName(element), True)
+            End If
+        Next
+        Return True
+    End Function
+
+
+End Class
